27ad9726bc0b60bdd8c628393323ebe3bda3b881
Adezb <temitopeceo@gmail.com>
2022-12-29 05:36:31 +0100

    First Update

---
+++ portfolio-frontend-react/src/constants/images.js
@@ -0,0 +1,53 @@
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
+import bgIMG from "../assets/bgIMG.png";
+import bgWhite from "../assets/bgWhite.png";
+import css from "../assets/css.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/react.png";
+import mu5 from "../assets/mu5.png";
+import vue from "../assets/vue.png";
+import profile1 from "../assets/profile1.png";
+import circle from "../assets/circle.svg";
+import adebayologo from "../assets/adebayologo.png";
+import webdesign from "../assets/webdesign.png";
+import frontend from "../assets/frontend.png";
+import graphicdsgn from "../assets/graphicdsgn.png";
+import git from "../assets/git.png";
+import adidas from "../assets/adidas.png";
+import amazon from "../assets/amazon.png";
+import bolt from "../assets/bolt.png";
+import flutter from "../assets/flutter.png";
+import mail from "../assets/mail.png";
+import smartphone from "../assets/smartphone.png";
+
+export default {
+  about01,
+  about02,
+  about03,
+  about04,
+  bgIMG,
+  bgWhite,
+  css,
+  html,
+  javascript,
+  react,
+  mu5,
+  vue,
+  profile1,
+  circle,
+  adebayologo,
+  webdesign,
+  frontend,
+  graphicdsgn,
+  git,
+  adidas,
+  amazon,
+  bolt,
+  flutter,
+  mail,
+  smartphone,
+};
